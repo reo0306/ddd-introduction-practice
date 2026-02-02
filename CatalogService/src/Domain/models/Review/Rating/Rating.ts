7f208078c452067af09fb246571187ff25b23752
reo0306 <reo19840306@gmail.com>
2026-02-03 00:14:20 +0900

    update value object for refactaring

--- CatalogService/src/Domain/models/Review/Rating/Rating.ts
+++ CatalogService/src/Domain/models/Review/Rating/Rating.ts
@@ -1,17 +1,15 @@
-import { isEqual } from "lodash";
+import { ValueObject } from "Domain/models/shared/ValueObject";
 
-export class Rating {
-    private readonly _value: number;
-
+type RatingValue = number;
+export class Rating extends ValueObject<RatingValue, Rating> {
     static readonly MAX = 5;
     static readonly MIN = 1;
 
-    constructor(value: number) {
-        this.validaete(value);
-        this._value = value;
+    constructor(value: RatingValue) {
+        super(value);
     }
 
-    private validaete(value: number): void {
+    protected validate(value: RatingValue): void {
         if (!Number.isInteger(value)) {
             throw new Error("評価は整数値でなければなりません.");
         }
@@ -19,14 +17,6 @@ export class Rating {
         if (value < Rating.MIN || value > Rating.MAX) {
             throw new Error(`評価は${Rating.MIN}から${Rating.MAX}の数値でなければなりません.`);
         }
-    }
-
-    equals(other: Rating): boolean {
-        return isEqual(this._value, other._value);
-    }
-
-    get value(): number {
-        return this._value;
     }
 
     getQualityFactor(): number {
